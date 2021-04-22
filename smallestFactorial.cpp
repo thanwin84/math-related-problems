ed782e72dd8ac9864b9755379830ba1e7c5824b8
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2021-04-22 15:26:04 +0600

    Create smallestFactorial.cpp

---
+++ smallestFactorial.cpp
@@ -0,0 +1,37 @@
+//formula is: floor(x/5) + floor(x/25)+......
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    bool check(int mid, int n){
+        int count = 0;
+        int f = 5;
+        while (f <= mid){
+            count += mid/f;
+            f = f * 5;
+        }
+        return count >= n;
+    }
+    int findNum(int n)
+    {
+        //complete the function here
+        if (n == 1){
+            return 5;
+        }
+        int start = 0;
+        int end = n*5;
+        int smallestFactorial = -1;
+        while (start <= end){
+            int mid = start + (end -start)/2;
+            if(check(mid, n)){
+                smallestFactorial = mid;
+                end = mid -1;
+            }
+            else {
+                start = mid + 1;
+            }
+        }
+        return smallestFactorial;
+    }
+};
