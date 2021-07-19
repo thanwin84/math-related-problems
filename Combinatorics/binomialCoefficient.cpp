a8d703862dd9d5a5aaea2125515f1eaaa6d50b3f
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2021-07-19 14:27:29 +0600

    Update binomialCoefficient.cpp

--- Combinatorics/binomialCoefficient.cpp
+++ Combinatorics/binomialCoefficient.cpp
@@ -58,3 +58,21 @@ public:
 		return dp[n][r];
 	}
 };
+//space optimized solution
+#define mod 1000000007
+using namespace std;
+int dp[801];
+class Solution {
+public:
+	int nCr(int n, int r) {
+		if (r > n) return 0;
+		memset(dp, 0, sizeof(dp));
+		dp[0] = 1;
+		for (int i = 1; i <= n; i++) {
+			for (int j = min(i, r); j > 0; j--) {
+				dp[j] = (dp[j - 1] + dp[j]) % mod;
+			}
+		}
+		return dp[r];
+	}
+};
